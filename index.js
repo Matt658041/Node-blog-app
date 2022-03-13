39944b15cf91355171592d70033eeed8822b0dcb
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-03-13 14:39:07 -0500

    expressinstallation

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-const http = require('http')
-const fs = require('fs')
-const homepage = fs.readFileSync('index.html')
-const aboutPage = fs.readFileSync('about.html')
-const contactPage = fs.readFileSync('contact.html')
-const notFoundPage =fs.readFileSync('notfound.html')
-
-const server = http.createServer((req, res) => {
-    if(req.url === '/about')
-    res.end('aboutPage') 
-    else if (req.url === '/contact')
-    res.end('contactPage')
-    else if(req.url === '/')
-    res.end('homePage')
-    else {
-        res.writeHead(404)
-        res.end('notFoundPage')
-    }
+const express = require ('express')
+const app = express ()
+app.listen(3000,()=> {
+    console.log("App listening on port 3000");
 })
 
-server.listen(3000)
 
