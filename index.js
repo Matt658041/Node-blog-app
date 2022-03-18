514dac7887b8e8e3e71f51441dd06a393605610e
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-03-18 09:33:57 -0500

    updated links to pages folder

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
+const path = require('path')
 const app = new express()
 app.use(express.static('public'))
 
 app.listen(4000, ()=> {
     console.log('App listening on port 4000');
+
 })
-
-
+app.get('/',(req,res) => {
+        res.sendFile(path.resolve(_dirname, 'pages/index.html'))
+    })
+app.get('/',(req,res) => {
+        res.sendFile(path.resolve(_dirname, 'pages/about.html'))
+    })
+app.get('/',(req,res) => {
+        res.sendFile(path.resolve(_dirname, 'pages/contact.html'))
+    })
+app.get('/',(req,res) => {
+        res.sendFile(path.resolve(_dirname, 'pages/post.html'))
+    })
 
